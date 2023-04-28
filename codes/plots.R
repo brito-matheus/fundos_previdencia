a383b7347bbfbe73995e17be91f4d272e85cffc2
Matheus Carrijo de Brito <110065290+brito-matheus@users.noreply.github.com>
2023-04-27 23:57:22 -0300

    update plots data set

--- codes/plots.R
+++ codes/plots.R
@@ -1,5 +1,72 @@
+#### Author: Matheus Carrijo de Brito 
+#### Construção de Previsão 
+#### Data: 27/04/2023
+
+rm(list = ls())
+
+library(readxl)
+library(tidyquant)
+library(quantmod) 
+library(tidyverse)
+library(data.table)
+library(stringr)
+library(kableExtra)
+library(fixest)
 
 
+# importando os dados de fatores 
+
+source("codes/importando_fatores.R")
+
+
+# Importando dados de fatores 
+
+
+dt_cot <- read_csv("../cotação.csv", 
+                   locale = locale(encoding = "latin1"), 
+                   col_names = c("ticker", "data", "close", "open", "min", "max",
+                                 "mean", "quant", "quant_tit", "vol"), skip = 1) |> 
+  select(ticker, data, close, quant, vol) |> 
+  mutate(ticker = str_remove(ticker, pattern = "<XBSP>")) |>
+  mutate(close = as.numeric(close),
+         quant = as.numeric(quant), 
+         vol = as.numeric(vol))
+
+dt_cot <- dt_cot |> 
+  mutate(ym = paste(year(data), month(data), sep = "_")) |> 
+  group_by(ym) |> 
+  mutate(data_da_competencia = last(data)) |> 
+  ungroup() |> 
+  group_by(ticker) %>%
+  mutate(index = match(ym, unique(ym))) %>%
+  ungroup() |> 
+  na.omit()
+
+
+dt_cot <- setDT(dt_cot) %>%
+  .[order(ticker, data), ]
+
+
+# Making the return
+dt_cot[ ,`:=`(ret = c(NA, diff(log(close)))), by = .(ticker)]
+
+dt_cot <- factors[dt_cot, on = "data"]
+
+dt_cot[, `:=`(ret_exc = ((ret - Risk_free) - Rm_minus_Rf))]
+
+# Construction of the future returns 
+
+dt_cot[, `:=`(ret_21 = frollapply(x = ret, n = 21, FUN = function(x) prod(1 + x, na.rm = T) - 1 , fill = NA, align = "right"),
+              ret_63 = frollapply(x = ret, n = 63, FUN = function(x) prod(1 + x, na.rm = T) - 1 , fill = NA, align = "right"),
+              ret_156 = frollapply(x = ret, n = 126, FUN = function(x) prod(1 + x, na.rm = T) - 1 , fill = NA, align = "right"),
+              ret_252 = frollapply(x = ret, n = 252, FUN = function(x) prod(1 + x, na.rm = T) - 1 , fill = NA, align = "right"), 
+              ret_21_exc = frollapply(x = ret_exc, n = 21, FUN = function(x) prod(1 + x, na.rm = T) - 1 , fill = NA, align = "right"), 
+              ret_63_exc = frollapply(x = ret_exc, n = 63, FUN = function(x) prod(1 + x, na.rm = T) - 1 , fill = NA, align = "right"),
+              ret_156_exc = frollapply(x = ret_exc, n = 126, FUN = function(x) prod(1 + x, na.rm = T) - 1 , fill = NA, align = "right"),
+              ret_252_exc = frollapply(x = ret_exc, n = 252, FUN = function(x) prod(1 + x, na.rm = T) - 1 , fill = NA, align = "right")
+), by = .(ticker)]
+
+# Manipulação de dados 
 
 df_grouped <- dt_cot %>% 
   select(ticker, data, ret_252) |> 
@@ -10,14 +77,17 @@ df_grouped <- dt_cot %>%
   arrange(ticker, data) %>% 
   ungroup()
 
-
+# Construction of the future return index data frame
 dt_temp_2 <- matrix(NA_real_, nrow = nrow(df_grouped), ncol = 252) |> 
   data.frame() 
 
-
+# binding the 
 dt_temp_2 <- cbind(df_grouped, dt_temp_2) |> setDT()
 
+# Name of the columns 
+cols <- sprintf("X%s", seq(1, 252, 1))
 
+# Special functions 
 special_subset <- function(x, dt_frame){
   
   vec_252 <- dt_frame[x:(x+252), .SD[c(1:252), .(return)]] |> t()
@@ -32,7 +102,12 @@ special_subset <- function(x, dt_frame){
 }
 
 special_reshape <- function(dt_frame){
-
+  
+  # Name of the columns 
+  cols <- sprintf("X%s", seq(1, 252, 1))
+  
+  
+  
   for(i in 1:nrow(dt_frame)){
     
     dt_frame[i, (cols) := special_subset(x = i, dt_frame = dt_frame)]
@@ -40,10 +115,11 @@ special_reshape <- function(dt_frame){
   }
 }
 
-
+# Split the data frame in by tickers
 df_list <- split(dt_temp_2, dt_temp_2$ticker)
-        
-              
+
+
+# Passing the function in a list of data frames 
 for(j in 1:length(df_list)){
   
   special_reshape(dt_frame = df_list[[j]]) 
@@ -53,6 +129,8 @@ for(j in 1:length(df_list)){
 
 # Test 
 
-do.call("rbind", df_list) |> setDT() %>% .[, .SD[c(1, .N)], by = .(ticker)] |> View()
+dt_cot_future <- do.call("rbind", df_list) 
 
+
+readr::write_csv(x = dt_cot_future, file = '../dados/dt_cot_future.csv')
 
